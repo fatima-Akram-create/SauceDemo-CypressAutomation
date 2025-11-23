bd67da6f38e0b33f47c227cd9e3070591a22a02f
fatima-Akram-create <testingthe456test@gmail.com>
2025-11-24 00:49:03 +0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.saucedemo.com",
-    pageLoadTimeout: 120000,
+    pageLoadTimeout: 0,
     defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {},
   },
